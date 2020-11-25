8b8591e435e2899b7b9fae350840b9637a318c08
m.nicolas <m.nicolas@rankr.fr>
2020-11-25 14:49:49 +0100

    pouet pouet chaud devant

--- travis/push.sh
+++ travis/push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git remote add cleverapps https://$CLEVER_TOKEN:CLEVER_SECRET@push.clever-cloud.com/app_YOUR_APPLICATION_ID.git
+git remote add cleverapps https://$CLEVER_TOKEN:CLEVER_SECRET@push.clever-cloud.com/app_03e1dd68-0565-441c-a6cc-9e57878fbe73.git
 git --verbose --force push cleverapps master
